240f34de1247761d753954ce8e29a27123d3590d
Daniel Asfaw <asfawdaniel779@gmail.com>
2025-02-11 03:08:06 +0300

    add middleware to backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("jobs.json"); // Make sure this file exists in the same directory
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({
+  static: false, // Disable serving static files from `/public`
+});
 
 server.use(middlewares);
 server.use(router);
