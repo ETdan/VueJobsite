60946404eb59790b243aa1b8658f02b376027b79
Daniel Asfaw <asfawdaniel779@gmail.com>
2025-02-11 03:21:36 +0300

    updated the json server endpoint in vite.config.js and change localhost:5000 in joblistings to real endpoint

--- JobSearchApp/src/components/JobListings.vue
+++ JobSearchApp/src/components/JobListings.vue
@@ -13,7 +13,7 @@ defineProps({
 
 onMounted(async () => {
   try {
-    const response = await axios.get("http://localhost:5000/jobs");
+    const response = await axios.get("/api/jobs");
     jobs.value = response.data;
   } catch {
     console.log("error fetching jobs");
