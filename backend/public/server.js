d50c79fc50dc206de315236d50ae9f08467b60d4
Daniel Asfaw <asfawdaniel779@gmail.com>
2025-02-11 03:39:12 +0300

    comment code

--- backend/public/server.js
+++ backend/public/server.js
@@ -1,9 +1,9 @@
 import { create, router as _router, defaults } from "json-server";
 const server = create();
 const router = _router("../jobs.json"); // Make sure this file exists in the same directory
-const middlewares = defaults({
-  static: false, // Disable serving static files from `/public`
-});
+// const middlewares = defaults({
+//   static: false, // Disable serving static files from `/public`
+// });
 
 server.use(middlewares);
 server.use(router);
